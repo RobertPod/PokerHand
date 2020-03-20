037fb2e93283e2b9e83bc6c5232f9d2db566ae9d
Robert Podsiadly <rob.podsiadly@gmail.com>
2020-03-20 21:48:10 +0100

    First three tests, last RED

---
+++ src/main/scala/PokerHand.scala
@@ -0,0 +1,15 @@
+object PokerHand {
+  def evaluate(hand: String): String = {
+    val (_, maxChar) = hand
+      .split(" ")
+      .map { card =>
+        card
+          .toList match {
+          case List('J', _) => (11, 'J')
+          case List(figure, _) => (figure.toString.toInt, figure)
+        }
+      }
+      .max
+    "high card : " + maxChar
+  }
+}
---
+++ src/test/scala/PokerHandTest.scala
@@ -0,0 +1,23 @@
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+
+class PokerHandTest extends AnyWordSpec with Matchers {
+
+  "Poker hand" should {
+    "pick the highest card 7" in {
+      PokerHand.evaluate("5H 4C 6S 7S 2D") should equal("high card : 7")
+    }
+    "pick the highest card 9" in {
+      PokerHand.evaluate("5H 4C 6S 9S 2D") should equal("high card : 9")
+    }
+
+    "pick the highest card J" in {
+      PokerHand.evaluate("5H 4C 6S JS 2D") should equal("high card : J")
+    }
+
+    "pick the highest card T" in {
+      PokerHand.evaluate("5H 4C 6S JS 2D") should equal("high card : T")
+    }
+  }
+}
